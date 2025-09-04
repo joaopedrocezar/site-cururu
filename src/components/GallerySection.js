c153efb77b4cf17f57f4a35abca080c23a2c9fe4
joaopedrocezar <joao.cezar@etec.sp.gov.br>
2025-09-04 20:17:44 -0300

    Update GallerySection.js

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -7,7 +7,7 @@ export default function GallerySection({ toadData }) {
         <h2 className="text-3xl md:text-4xl font-extrabold text-center mb-12 scroll-animate">Galeria de Fotos</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {toadData.gallery.map((image, index) => (
-            <div key={index} className="group overflow-hidden rounded-lg shadow-lg scroll-animate" style={{ animationDelay: `${index * 100}ms` }}>
+            <div key={index} className="group overflow-hidden rounded-lg shadow-lg scroll-animate h-64" style={{ animationDelay: `${index * 100}ms` }}>
               <div className="relative w-full h-full">
                 <Image src={image} alt={`Imagem da galeria ${index + 1}`} fill={true} style={{ objectFit: 'cover' }} className="transition-transform duration-500 ease-in-out group-hover:scale-110"/>
               </div>
